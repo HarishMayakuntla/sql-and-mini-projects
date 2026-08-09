ec213b684462ad56d4a9068cadf21c2b3163e8a7
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-08-09 12:16:46 +0530

    Added constaints practice

---
+++ 04_constarints/04_unique.sql
@@ -0,0 +1,35 @@
+--=================================== UNIQUE Constraint in SQL ===============================
+
+-- The UNIQUE constraint ensures that duplicate values are not allowed in a column.
+
+
+CREATE TABLE employees (
+    id INT,
+    name VARCHAR(50),
+    email VARCHAR(100) UNIQUE
+);
+
+
+--valid 
+
+INSERT INTO employees VALUES (1, 'Harish', 'harish@gmail.com');
+INSERT INTO employees VALUES (2, 'Rohit', 'rohit@gmail.com');
+
+-- in valid
+
+INSERT INTO employees VALUES (3, 'Anil', 'harish@gmail.com');
+
+
+-- =========================== UNIQUE vs PRIMARY KEY =========================
+
+
+| UNIQUE                                       | PRIMARY KEY               |
+| -------------------------------------------- | ------------------------- |
+| Prevents duplicate values                    | Prevents duplicate values |
+| Can generally allow `NULL` depending on DBMS | Cannot contain `NULL`     |
+| Multiple `UNIQUE` constraints allowed        | Only one primary key      |
+| Used for values like email, phone            | Used to identify each row |
+
+
+
+UNIQUE = No duplicate values
