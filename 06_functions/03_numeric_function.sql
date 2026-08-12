6a3282904db0e0e346e7b34369acf2fa7f1c1295
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-08-12 12:24:39 +0530

    Added function in sql practice

---
+++ 06_functions/03_numeric_function.sql
@@ -0,0 +1,43 @@
+-- ======================== Numeric Functions =============================
+
+-- These functions work with numbers.
+
+-- Work on one row at a time and return one result for each row.
+
+
+/* 
+
+ROUND() =  Rounds a number
+CEIL() = Rounds a number up.
+FLOOR() = Rounds a number down
+ABS() = Returns the absolute value
+MOD() = MOD is used to find the remainder after division
+
+
+*/
+
+
+
+SELECT ROUND(45.6789, 2);  -- o/p:45.68
+
+
+SELECT CEIL(45.2); -- o/p : 46
+
+
+SELECT FLOOR(45.9); -- o/p : 45
+
+
+SELECT ABS(-50); --o/p : 50
+
+
+SELECT MOD(10, 3); -- o/p :1
+
+
+
+
+
+
+
+
+
+
